d329f10519172496ac06398ed793ae17a2549832
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-05-19 10:23:53 -0300

    20 - Movendo o Container para seu próprio React Component

---
+++ chronos-pomodoro/src/components/Container.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Container.module.css'
+
+type ContainerProps = {
+    children: React.ReactNode
+}
+
+const Container = ({children}:ContainerProps) => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.content}>
+          <section>
+            {children}
+          </section>
+        </div>
+      </div>
+  )
+}
+
+export default Container
