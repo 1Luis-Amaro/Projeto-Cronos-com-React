a842b84c91714811b1078dd5af7a8fb856270408
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-05-30 17:52:33 -0300

    comments

--- chronos-pomodoro/src/models/TaskStateModel.ts
+++ chronos-pomodoro/src/models/TaskStateModel.ts
@@ -1,14 +1,16 @@
 import type { TaskMode } from "./TaskModel"
 
 export type TaskStateModel = {
-    task: TaskMode[];
-    secondsRemaining: number;
-    formattedSecondsRemaining: string;
-    activeTask: TaskMode | null
-    currentCycle: number; //é as bolinhas que vão de 1 a 8
+    task: TaskMode[]; //preciso no histórico, Mainform
+    secondsRemaining: number; //vou precisar no CountDown, Histórico, MainForm, Button
+    formattedSecondsRemaining: string; // vou precisa no Título, CountDown
+    activeTask: TaskMode | null //CountDown, His
+    currentCycle: number; //é as bolinhas que vão de 1 a 8, vou precisar na Home
     config: {
         workTime: number;
         shortBreakTime: number;
         longBreakTime: number;
     }
 }
+
+
