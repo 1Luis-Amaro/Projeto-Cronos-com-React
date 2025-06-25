a1446e294ba9990195cd7a75605e0898ca5eb389
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-06-25 11:04:54 -0300

    46 -  Context API - criando um context com createContext, useContext + Provider

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,11 +1,33 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import AboutPomodoro from "./pages/AboutPomodoro"
+import type { TaskStateModel } from "./models/TaskStateModel";
+import { useState } from "react";
+import Home from "./pages/Home";
+import { TaskContext } from "./contexts/TaskContext";
 
+const initialState: TaskStateModel = {
+  task: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime:25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  }
+}
 
 function App() {
-  return <AboutPomodoro/>
+  const [state, setState] = useState(initialState)
+  return(
+  <TaskContext.Provider value={{outraCoisa:321}}>
+     <Home/>
+  </TaskContext.Provider>
+  )
 }
+
+
 
 
 export default App;
--- chronos-pomodoro/src/components/CountDown/index.tsx
+++ chronos-pomodoro/src/components/CountDown/index.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import styles from "./styles.module.css";
+import { TaskContext } from "../../contexts/TaskContext";
 
 const CountDown = () => {
+  const taskContext = useContext(TaskContext);
+  console.log(taskContext)
   return <div className={styles.container}>00:00</div>;
 };
 
