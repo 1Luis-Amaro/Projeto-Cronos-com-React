70f1cdc26e93c56114a42e65ebf66131595fb9be
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-07-04 16:48:37 -0300

    51 - Input controlado no React com value, onChange e useState

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -2,9 +2,11 @@ import { PlayCircleIcon } from "lucide-react";
 import DefaultButton from "../DefaultButton";
 import DefaultInput from "../DefaultInput";
 import { Cycles } from "../Cycles";
+import { useState } from "react";
 
 export function MainForm() {
- 
+  const [taskName, setTaskName] = useState('')
+
   function handleCreateNewTask (event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     console.log("deu certo")
@@ -12,12 +14,15 @@ export function MainForm() {
 
     return (
          <form onSubmit={ handleCreateNewTask}  className="form">
+          <h1>{taskName}</h1>
           <div className="formRow">
             <DefaultInput
               labelText="task"
               id="meuInput"
               type="text"
               placeholder="Digite algo"
+              value={taskName}
+              onChange={(e) => setTaskName(e.target.value)}
               //disabled
               //defaultValue='Valor preenchido'
             />
