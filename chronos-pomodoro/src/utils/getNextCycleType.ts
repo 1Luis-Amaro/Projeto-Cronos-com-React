02f5e098708487bb5df7c2a6aa36a44f259af3e1
1Luis-Amaro <luis.tavio03@gmail.com>
2025-07-22 20:49:41 -0300

    56 - state.config em duration da task (workTime, shortBreakTime, longBreakTime)

--- chronos-pomodoro/src/utils/getNextCycleType.ts
+++ chronos-pomodoro/src/utils/getNextCycleType.ts
@@ -2,6 +2,6 @@ import type { TaskMode } from "../models/TaskModel";
 
 export function getNextCycleType (currentCycle: number) : TaskMode['type'] {
     if (currentCycle % 8 === 0) return 'longBreakTime';
-    if (currentCycle % 2 === 0) return 'shortBreak';
+    if (currentCycle % 2 === 0) return 'shortBreakTime';
     return 'workTime';
 }
