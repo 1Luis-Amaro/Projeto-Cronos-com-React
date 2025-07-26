d2eb62d8d38ed4d68abd501e3e1e3a3368df6e87
1Luis-Amaro <luis.tavio03@gmail.com>
2025-07-26 11:41:56 -0300

    63 - Entendendo useReducer, reducer, dispatch e actions para trabalhar com o estado

--- chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,21 +1,37 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { initialTaskState } from "./initialTaskState"
 import { TaskContext } from "./TaskContext"
 
 type TaskContextProviderProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-export function TaskContextProvider({children} : TaskContextProviderProps) {
-      const [state, setState] = useState(initialTaskState)
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, setState] = useState(initialTaskState)
 
-      useEffect(() => {
-        console.log(state)
-      },[state])
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action)
 
-    return (
-        <TaskContext.Provider value={{state,setState}}>
-        {children}
-        </TaskContext.Provider>
-    )
+    switch (action) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT': return state - 1;
+      case 'DELETE': return  0
+    }
+
+    return state
+  }, 0)
+
+  // useEffect(() => {
+  //  console.log(state)
+  // },[state])
+
+  return (
+    <TaskContext.Provider value={{ state, setState }}>
+      <h1>O numero é {numero} </h1>
+      <button onClick={() => { dispatch('INCREMENT') }} >Incrementar</button>
+      <button onClick={() => { dispatch('DECREMENT') }} >decrementar</button>
+      <button onClick={() => { dispatch('DELETE') }} >Apagar</button>
+    </TaskContext.Provider>
+  )
 }
