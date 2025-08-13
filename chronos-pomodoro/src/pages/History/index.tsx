ec70e94a019adc1c1cce1bc67d036f90c5e72ce5
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-08-13 12:14:33 -0300

    90 - Corrigindo um bug ao tentar limpar o histórico fora da página

--- chronos-pomodoro/src/pages/History/index.tsx
+++ chronos-pomodoro/src/pages/History/index.tsx
@@ -47,6 +47,12 @@ const History = () => {
     dispatch({type: TaskActionTypes.RESET_STATE})
   },[confirmClearHistory, dispatch])
 
+  useEffect(() => {
+    return () => {
+      showMessage.dismiss()
+    }
+  },[])
+
 
   function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
     const newDirection = sortTasksOptions.direction === "desc" ? "asc" : "desc";
